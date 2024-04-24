a99bbc5cd2b88ec0bcad33c6c45d812c7bb92386
Victor Queiroga <victorqueirogacarvalho@gmail.com>
2024-04-24 18:15:53 -0300

    Create User Repository

---
+++ src/main/java/com/develcode/demo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.develcode.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.develcode.demo.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+}
